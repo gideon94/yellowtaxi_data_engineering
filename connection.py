5d94c71c838f5601c650583d77a5339beb0a1b8d
Ganesh <30742929+thomkallor@users.noreply.github.com>
2020-02-15 15:41:40 +0000

    achenged constants

--- connection.py
+++ connection.py
@@ -2,6 +2,6 @@ from constants import ACTIVEMQ_PORT
 import stomp
 
 def create_connection():
-    conn = stomp.Connection(host_and_ports=[('192.168.137.1', ACTIVEMQ_PORT)])
+    conn = stomp.Connection(host_and_ports=[('localhost', ACTIVEMQ_PORT)])
     conn.connect(login='system', passcode='manager', wait=True)
     return conn
--- subscriber.py
+++ subscriber.py
@@ -3,6 +3,7 @@ import stomp
 
 class Subscriber:
     def subscribe(self, destination, id, listener):
+        self.exit=False
         self.conn= create_connection()
         self.conn.set_listener('clean', listener)
         self.conn.subscribe(destination=destination, id=id, ack='auto')
