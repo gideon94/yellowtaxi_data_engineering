85835c7a3217d9ac89944abcc2c5fdf5a5a0cf2f
gideon94 <madhanmax94@gmail.com>
2020-02-16 17:23:34 +0000

    Analytics done

--- join_tables.py
+++ join_tables.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd
+import datetime
 
 from listener import Listener
 from publisher import Publisher
@@ -9,31 +10,51 @@ from constants import MVC_PATH
 from constants import LOOKUP_PATH
 
 publisher=Publisher()
-
+lookup_df = pd.read_csv('LOOKUP_PATH')
+mvc_df = pd.read_csv('MVC_PATH')
 
 def hourly_action(msg):
     ##publish the hourly results
-    publisher.publish(json.dumps(msg), '/queue/report/hour')
-    if 
+    global lookup_df
+    filter_list = msg['peak_zones']
+    filter_lookup = lookup_df[lookup_df['LocationID'].isin(filter_list)]
+    filter_lookup = filter_lookup[['Borough','Zone']]
+
+    mvc_df['DATE'] = pd.to_datetime(mvc_df['CRASH DATE'] + ' ' + mvc_df['CRASH TIME'])
+    time = datetime.datetime.strptime(time, '%d-%m-%Y %H:%M:%S')
+    time_ = time + datetime.timedelta(hours = 1)
+    from_time = '{:%Y-%m-%d %H:00:00}'.format(time)
+    to_time = '{:%Y-%m-%d %H:00:00}'.format(time_)
+
+    filter_mvc = mvc_df.loc[(mvc_df['DATE'] > from_time) & (mvc_df['DATE'] < to_time) & mvc_df['BOROUGH'].isin(filter_lookup['Borough'])]
+    filter_mvc = filter_mvc.dropna(subset=['BOROUGH'])
+    filter_mvc = filter_mvc[['BOROUGH','LOCATION','CONTRIBUTING FACTOR VEHICLE 1']]
+
+    freq_report={'zones':filter_lookup.to_json(orient='records'), 'freq':msg['trips']},
+    publisher.publish(freq_report, '/queue/report/hour')
+
+    accident_report={'accident report':filter_mvc.to_json(orient = 'records')},
+    publisher.publish(msg,'/queue/report/hour')
 
 def daily_action(msg):
     ##publish the daily results
-    publisher.publish(json.dumps(msg), '/queue/report/day')
-    pass
+
+    mvc_df['DATE'] = pd.to_datetime(mvc_df['CRASH DATE'] + ' ' + mvc_df['CRASH TIME'])
+    time = datetime.datetime.strptime(time, '%d-%m-%Y %H:%M:%S')
+    time_ = time + datetime.timedelta(hours = 1)
+    from_time = '{:%Y-%m-%d %H:00:00}'.format(time)
+    to_time = '{:%Y-%m-%d %H:00:00}'.format(time_)
+
+    filter_mvc = mvc_df.loc[(mvc_df['DATE'] > from_time) & (mvc_df['DATE'] < to_time)]
+    filter_mvc = filter_mvc.dropna(subset=['BOROUGH'])
+    filter_mvc = filter_mvc[['BOROUGH','LOCATION','CONTRIBUTING FACTOR VEHICLE 1']]
+
+    accident_report={'accident report':filter_mvc.to_json(orient = 'records')},
+    publisher.publish(msg,'/queue/report/day')
+
 
 def main():
     ##read the crash and lookup csv
-    mvc_df = pd.read_csv('MVC_PATH')
-    lookup_df = pd.read_csv('LOOKUP_PATH')
-    
-
-
-    joined_data={
-    'tpep_pickup_datetime':message['tpep_pickup_datetime'],
-    'tpep_dropoff_datetime':message['tpep_dropoff_datetime'],
-    'PULocationID':message['PULocationID'],
-    'DOLocationID':message['DOLocationID']
-    }
     
     ##join with the tables
     hour_subscription=Subscriber()
--- test.py
+++ test.py
@@ -1,11 +1,62 @@
 import pandas as pd
+import datetime
 
 mvc_df = pd.read_csv('C:/Users/Gideon/Desktop/RTAI/dataset/mvc.csv')
-lookup_df = pd.read_csv('C:/Users/Gideon/Desktop/RTAI/dataset/lookup.csv')
+# lookup_df = pd.read_csv('C:/Users/Gideon/Desktop/RTAI/dataset/lookup.csv')
 
-filter_list = [11,12,13,14,20,19,17]
-filter_df = lookup_df[lookup_df['LocationID'].isin(filter_list)]
-filter_df = filter_df[['LocationID', 'Borough', 'Zone']]
+# filter_list = [11,12,13,14,20,19,17]
 
-print(filter_df)
-print(lookup_df.shape, filter_df.shape)
+# filter_df = lookup_df[lookup_df['LocationID'].isin(filter_list)]
+# filter_df = filter_df[['Borough','Zone']]
+# print(filter_df.to_json(orient='records'))
+times = ['01-01-2018 00:21:00','01-01-2018 01:21:00']
+times_from = pd.to_datetime(times)
+times_to = times_from + pd.DateOffset(hours=1)     
+mvc_df['DATE'] = pd.to_datetime(mvc_df['CRASH DATE'] + ' ' + mvc_df['CRASH TIME'])
+
+mvc_df = mvc_df['DATE'].loc[times_from[0]:times_to[0]]
+print(mvc_df.head())
+
+
+# mvc_df = mvc_df.loc[mvc_df['DATE']> times_from & mvc_df['DATE']< times_to]
+
+# print(mvc_df.head())
+
+
+# for t in times:
+#      date = datetime.datetime.strptime(t, '%d-%m-%Y %H:%M:%S')
+#      date_time_obj_ = date_time_obj + datetime.timedelta(hours = 1)
+#      d = '{:%d-%m-%Y %H:00:00}'.format(date_time_obj)
+#      d_ = '{:%d-%m-%Y %H:00:00}'.format(date_time_obj_)
+#      date.append(d)
+#      date_.append(d_)
+
+# print(date,date_)
+# filter_mvc = mvc_df[mvc_df['DATE'].between(date,date_)]
+
+# # date = []
+# # date_ = []
+# # time = []
+# # time_ = []
+# # for t in times:
+# #     date_time_obj = datetime.datetime.strptime(t, '%d-%m-%Y %H:%M:%S')
+# #     date_time_obj_ = date_time_obj + datetime.timedelta(hours = 1)
+# #     d_ = '{:%d-%m-%Y}'.format(date_time_obj_)
+# #     d = '{:%d-%m-%Y}'.format(date_time_obj)
+# #     t='{:%H:%M}'.format(date_time_obj)
+# #     t_='{:%H:%M}'.format(date_time_obj_) 
+# #     date.append(d)
+# #     date_.append(d_)
+# #     time.append(t)
+# #     time_.append(t_)
+# # print(date, date_,time,time_)
+
+# # mvc_df['CRASH DATE'] = pd.to_datetime(mvc_df['CRASH DATE'])
+# # mvc_df['CRASH TIME'] = pd.to_datetime(mvc_df['CRASH TIME'])
+# # filter_mvc = mvc_df[mvc_df['CRASH DATE'].isin(date)]
+# # filter_mvc = filter_mvc.loc[filter_mvc['CRASH TIME']> time[0] & filter_mvc['CRASH TIME']< time_[0]]
+# # print(filter_mvc)
+# # mvc_df['CRASH DATE'] = pd.to_datetime(mvc_df['CRASH DATE'])
+# # filter_mvc = mvc_df[mvc_df['CRASH DATE'].isin(date)]
+# # filter_mvc = filter_mvc[filter_mvc['CRASH TIME'].between(time[0],time_[0])]
+# # print(filter_mvc)
